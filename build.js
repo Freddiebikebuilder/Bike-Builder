d702ffb2d905b3df43415b5cf6bd366d58cb00ba
Freddiebikebuilder <fh230408@gmail.com>
2025-07-12 12:28:51 +0100

    Update build.js

--- build.js
+++ build.js
@@ -1,1 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const selectedFrame = localStorage.getItem('selectedFrame');
+
+  if (selectedFrame) {
+    const frameImg = document.getElementById('selectedFrame');
+    frameImg.src = `Images/Frames/${selectedFrame}.webp`;
+    frameImg.alt = selectedFrame.replace(/-/g, ' ');
+  } else {
+    alert('No frame selected. Redirecting back.');
+    window.location.href = 'index.html';
+  }
+});
 
