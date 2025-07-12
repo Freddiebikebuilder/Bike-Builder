7f716930762004b9d733f2b526916f6d29b1c056
Freddiebikebuilder <fh230408@gmail.com>
2025-07-12 13:07:57 +0100

    Update build.js

--- build.js
+++ build.js
@@ -24,13 +24,12 @@ document.addEventListener('DOMContentLoaded', () => {
         forkImg.alt = img.alt;
       }
 
-      if (partType === 'wheels') {
-        const wheelsImg = document.getElementById('wheelsImage');
-        wheelsImg.src = `Images/Parts/Wheels/Overlays/${overlayFile}`;
-        wheelsImg.alt = img.alt;
-      }
-
-      // Add more part types here as needed
+      // Example for wheels overlay if you add wheels overlay img tag and id
+      // if (partType === 'wheels') {
+      //   const wheelsImg = document.getElementById('wheelsImage');
+      //   wheelsImg.src = `Images/Parts/Wheels/Overlays/${overlayFile}`;
+      //   wheelsImg.alt = img.alt;
+      // }
     });
   });
 });
